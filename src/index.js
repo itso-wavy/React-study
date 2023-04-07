9ebf22e278ea0c1c9779f066d0b7bdc77c4bc371
itso-wavy <you.meet.vvavy@gmail.com>
2023-04-07 16:56:38 +0900

    styles

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const container = document.getElementById('root');
-// ReactDOM.render(App, container)
-const root = createRoot(container);
-root.render(<App />);
