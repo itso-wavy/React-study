c1c390a7a458612fb3ad3197d5648325250f3793
itso-wavy <you.meet.vvavy@gmail.com>
2023-04-16 00:54:52 +0900

    refact: useConfirm

--- nomadcoder-hooks/src/hooks/useConfirm.jsx
+++ nomadcoder-hooks/src/hooks/useConfirm.jsx
@@ -1,9 +1,14 @@
 const useConfirm = (message = '', onConfirm, onCancle) => {
-  if (!onConfirm || typeof onConfirm !== 'function') return;
+  if (typeof onConfirm !== 'function') return;
   if (onCancle && typeof onCancle !== 'function') return;
   const confirmAction = () => {
     if (window.confirm(message)) onConfirm();
-    else onCancle();
+    else
+      try {
+        onCancle();
+      } catch {
+        return;
+      }
   };
 
   return confirmAction;
