8a5a4c5eb121aac8695a7497b07d317754cdcbbf
itso-wavy <you.meet.vvavy@gmail.com>
2024-03-28 19:56:26 +0900

    .

---
+++ react-complete-guide/24-tanstack-query/01-configuring-query-behaviors/src/components/util/http.jsx
@@ -0,0 +1,17 @@
+export async function fetchEvents(searchTerm) {
+  let url = 'http://localhost:3000/events';
+  if (searchTerm) url += url + searchTerm;
+
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the events');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { events } = await response.json();
+
+  return events;
+}
