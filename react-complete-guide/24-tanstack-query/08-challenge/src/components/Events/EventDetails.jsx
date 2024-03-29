33d46232cf8654b21ff04760f65cda6f96a987a6
itso-wavy <you.meet.vvavy@gmail.com>
2024-03-29 14:38:41 +0900

    .

---
+++ react-complete-guide/24-tanstack-query/08-challenge/src/components/Events/EventDetails.jsx
@@ -0,0 +1,100 @@
+import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
+import { useMutation, useQuery } from '@tanstack/react-query';
+
+import Header from '../Header.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { fetchEvent, deleteEvent, queryClient } from '../../util/http.jsx';
+
+export default function EventDetails() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['event', { id }],
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+  });
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['events'],
+        refetchType: 'none',
+      });
+      navigate(-1);
+    },
+  });
+
+  const handleDelete = () => {
+    mutate({ id });
+  };
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <div id='event-details-content' className='center'>
+        <p>Fetching event data...</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <div id='event-details-content' className='center'>
+        <ErrorBlock
+          title='Failed to load event'
+          message={
+            error.info?.message ||
+            'Failed to fetch event data. please try again later.'
+          }
+        />
+      </div>
+    );
+  }
+
+  if (data) {
+    const { date, description, image, location, title, time } = data;
+
+    const formattedDate = new Date(date).toLocaleDateString('en-US', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+    });
+
+    content = (
+      <>
+        <header>
+          <h1>{title}</h1>
+          <nav>
+            <button onClick={handleDelete}>Delete</button>
+            <Link to='edit'>Edit</Link>
+          </nav>
+        </header>
+        <div id='event-details-content'>
+          <img src={`http://localhost:3000/${image}`} alt={title} />
+          <div id='event-details-info'>
+            <div>
+              <p id='event-details-location'>{location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {formattedDate} @ {time}
+              </time>
+            </div>
+            <p id='event-details-description'>{description}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Outlet />
+      <Header>
+        <Link to='/events' className='nav-item'>
+          View all Events
+        </Link>
+      </Header>
+      <article id='event-details'>{content}</article>
+    </>
+  );
+}
